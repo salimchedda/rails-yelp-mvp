dab789ba7280c98e3391265e20a462571c05ecf7
Salim Cheddadi <salim.cheddadi@gmail.com>
2019-11-13 16:48:46 +0100

    exercise two

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_113841) do
+ActiveRecord::Schema.define(version: 2019_11_13_114836) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all if Rails.env.development?
+
+Restaurant.create!(
+  name: 'La brigade',
+  address: 'rue Oberkampf',
+  phone_number: '01 02 03 04 05',
+  category: 'french'
+)
+
+Restaurant.create!(
+  name: 'Esprit Chai',
+  address: 'rue Oberkampf',
+  phone_number: '02 03 04 05 06',
+  category: 'chinese'
+)
+
+Restaurant.create!(
+  name: '404',
+  address: 'rue des Gravilliers',
+  phone_number: '03 04 05 06 07',
+  category: 'italian'
+)
+
+Restaurant.create!(
+  name: 'SOMA',
+  address: 'rue de Mazarine',
+  phone_number: '04 05 06 07 08',
+  category: 'japanese'
+)
+
+Restaurant.create!(
+  name: 'HolyBelly',
+  address: 'rue des Petites Ecuries',
+  phone_number: '05 06 07 08 09',
+  category: 'belgian'
+)
